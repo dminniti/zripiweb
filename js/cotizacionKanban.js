395c663aeb7a668000b5d8c8282ecd1b965ed278
Damian Minniti <damianminniti@Damians-MBP.fibertel.com.ar>
2024-03-29 18:45:08 -0300

    Fix okr course + OKR in index

--- js/cotizacionKanban.js
+++ js/cotizacionKanban.js
@@ -3,10 +3,15 @@ function obtenerCotizacion() {
         .then(response => response.json())
         .then(data => {
             let precioPesosPromo = new Intl.NumberFormat("de-DE").format(document.getElementById('promo').value * parseInt(parseInt(data.compra) / 10) * 10);
-            let precioPesosRegular = new Intl.NumberFormat("de-DE").format(document.getElementById('regular').value * parseInt(parseInt(data.compra) / 10) * 10);
 
             document.getElementById('precioPromo').innerHTML = `${precioPesosPromo} ARS`;
-            document.getElementById('precioRegular').innerHTML = `${precioPesosRegular} ARS`;
+
+            if (document.getElementById('regular')) {
+                let precioPesosRegular = new Intl.NumberFormat("de-DE").format(document.getElementById('regular').value * parseInt(parseInt(data.compra) / 10) * 10);
+                document.getElementById('precioRegular').innerHTML = `${precioPesosRegular} ARS`;
+            }
+
+
         })
         .catch(error => {
             console.error('Error al obtener la cotización:', error);
--- js/cotizacionOtros.js
+++ js/cotizacionOtros.js
@@ -3,10 +3,13 @@ function obtenerCotizacion() {
         .then(response => response.json())
         .then(data => {
             let precioPesosPromo = new Intl.NumberFormat("de-DE").format(document.getElementById('promo').value * parseInt(parseInt(data.compra) / 10) * 10);
-            let precioPesosRegular = new Intl.NumberFormat("de-DE").format(document.getElementById('regular').value * parseInt(parseInt(data.compra) / 10) * 10);
+
+            if (document.getElementById('regular')) {
+                let precioPesosRegular = new Intl.NumberFormat("de-DE").format(document.getElementById('regular').value * parseInt(parseInt(data.compra) / 10) * 10);
+                document.getElementById('precioRegular').innerHTML = ` | ${precioPesosRegular} ARS`;
+            }
 
             document.getElementById('precioPromo').innerHTML = ` | ${precioPesosPromo} ARS`;
-            document.getElementById('precioRegular').innerHTML = ` | ${precioPesosRegular} ARS`;
         })
         .catch(error => {
             console.error('Error al obtener la cotización:', error);
